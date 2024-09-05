ed365d672681a815d5ee3014ba26415414493deb
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-05 23:37:39 +0530

    Add SelectField component

---
+++ src/components/SelectField.jsx
@@ -0,0 +1,32 @@
+import React, { useId } from "react";
+import PropTypes from "prop-types";
+
+function SelectField({ options, label, className = "", ...props }, ref) {
+  const id = useId();
+
+  return (
+    <div className="w-full">
+      {label && <labe htmlFor={id} className=""></labe>}
+      <select
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        id={id}
+        {...props}
+        ref={ref}
+      >
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+SelectField.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  label: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default React.forwardRef(SelectField);
