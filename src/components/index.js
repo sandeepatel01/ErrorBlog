731f99fbca46e6e113cdc5a89c8eab4dce00dbb7
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-05 18:30:14 +0530

    Add components

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./container/container";
+
+export {
+     Header,
+     Footer,
+     Logo,
+     Container,
+     LogoutBtn
+}
