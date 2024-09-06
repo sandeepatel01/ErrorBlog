2d6b5bba7935658452b1089ec54fef375beef48c
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-09-06 20:23:11 +0530

    Add components

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,28 @@
+import appwriteService from "../appwrite/config.service";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function PostCard({ $id, title, featuredImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl"
+          />
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+    </Link>
+  );
+}
+
+PostCard.propTypes = {
+  $id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  featuredImage: PropTypes.string.isRequired,
+};
+
+export default PostCard;
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,17 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./container/container";
+import Button from "./Button";
+import InputField from "./InputField";
+import RTE from "./RTE";
 
 export {
      Header,
      Footer,
      Logo,
      Container,
-     LogoutBtn
+     LogoutBtn,
+     Button,
+     InputField,
+     RTE
 }
